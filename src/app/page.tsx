a65843e6a63eb9f29fcea9901e94dce961094910
Gustavo Alexandre <gustavoslyth1@gmail.com>
2023-08-29 14:50:30 -0300

    ajuste em conversao

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,14 @@ import brazilianFlag from '../../public/brazilian_flag.svg';
 import { useEffect, useState } from 'react'
 import api from '../services/api'
 
+import USDflag from '../../public/usd_flag.svg';
+import SUICAflag from '../../public/suica_flag.svg';
+import JAPANflag from '../../public/japan_flag.svg';
+import UKflag from '../../public/uk_flag.svg';
+import ARSflag from '../../public/argentina_flag.svg';
+import Europeflag from '../../public/europe_flag.svg'
 export default function Home() {
-  const [inputValue, setInputValue] = useState(0);
+  const [inputValue, setInputValue] = useState<number | undefined>(undefined);
   const [dolarUSD, setDolarUSD] = useState<number | undefined>(undefined);
   const [libraEsterlinaGBP, setLibraEsterlinaGBP] = useState('');
   const [euroEUR, setEuroEUR] = useState('');
@@ -33,13 +39,101 @@ export default function Home() {
       }
     }
   };
+  const handleConvertLibraEsterlinaGBP = async () => {
+    if (inputValue !== undefined) {
+      const requestData = {
+        moedaOrigem: 'BRL',
+        moedaDestino: 'GBP',
+        moedaConvertida: inputValue
+      };
+      
+      try {
+        const response = await api.post('/convert', requestData);
+        setLibraEsterlinaGBP(response.data.valorConvertido);
+      } catch (error) {
+        console.error('Erro:', error);
+      }
+    }
+  };
+
+  
+  const handleConvertEuroEUR = async () => {
+    if (inputValue !== undefined) {
+      const requestData = {
+        moedaOrigem: 'BRL',
+        moedaDestino: 'EUR',
+        moedaConvertida: inputValue
+      };
+      
+      try {
+        const response = await api.post('/convert', requestData);
+        setEuroEUR(response.data.valorConvertido);
+      } catch (error) {
+        console.error('Erro:', error);
+      }
+    }
+  };
+  const handleConvertIeneJaponesJPY = async () => {
+    if (inputValue !== undefined) {
+      const requestData = {
+        moedaOrigem: 'BRL',
+        moedaDestino: 'JPY',
+        moedaConvertida: inputValue
+      };
+      
+      try {
+        const response = await api.post('/convert', requestData);
+        setIeneJaponesJPY(response.data.valorConvertido);
+      } catch (error) {
+        console.error('Erro:', error);
+      }
+    }
+  };
+
+  const handleConvertIenePesoArgentinoARS = async () => {
+    if (inputValue !== undefined) {
+      const requestData = {
+        moedaOrigem: 'BRL',
+        moedaDestino: 'ARS',
+        moedaConvertida: inputValue
+      };
+      
+      try {
+        const response = await api.post('/convert', requestData);
+        setPesoArgentinoARS(response.data.valorConvertido);
+      } catch (error) {
+        console.error('Erro:', error);
+      }
+    }
+  };
+  const handleConvertFrancoSuicoCHF = async () => {
+    if (inputValue !== undefined) {
+      const requestData = {
+        moedaOrigem: 'BRL',
+        moedaDestino: 'CHF',
+        moedaConvertida: inputValue
+      };
+      
+      try {
+        const response = await api.post('/convert', requestData);
+        setFrancoSuicoCHF(response.data.valorConvertido);
+      } catch (error) {
+        console.error('Erro:', error);
+      }
+    }
+  };
 
   useEffect(() => {
     const interval = setInterval(() => {
       if (inputValue !== undefined) {
         handleConvertDolarUSD();
+        handleConvertLibraEsterlinaGBP();
+        handleConvertEuroEUR();
+        handleConvertIeneJaponesJPY();
+        handleConvertIenePesoArgentinoARS();
+        handleConvertFrancoSuicoCHF();
       }
-    }, 2000); // Intervalo de 2 segundos
+    }, 10000);
 
     return () => clearInterval(interval);
   }, [inputValue]);
@@ -50,21 +144,24 @@ export default function Home() {
       value: dolarUSD,
       borderRadius: '10px',
       background: 'linear-gradient(92deg, #3C3B6E 0%, #B22234 100%)',
-      textColor: 'white'
+      textColor: 'white',
+      flag: USDflag
     },
     {
       coin: 'Euro (EUR)',
       value: euroEUR,
       borderRadius: '10px',
       background: 'linear-gradient(92deg, #033699 44.73%, #FC0 100%)',
-      textColor: 'white'
+      textColor: 'white',
+      flag: Europeflag
     },
     {
       coin: 'Libra Esterlina (GBL)',
       value: libraEsterlinaGBP,
       borderRadius: '10px',
       background: 'linear-gradient(92deg, #C8102E 0%, #012169 100%)',
-      textColor: 'white'
+      textColor: 'white',
+      flag: UKflag
     },
     {
       coin: 'Iene Japonês (YEN)',
@@ -72,21 +169,24 @@ export default function Home() {
       borderRadius: '10px',
       background: '#FFF',
       boxShadow: '2px 2px 4px 0px rgba(246, 0, 0, 0.25)',
-      textColor: '#F00000'
+      textColor: '#F00000',
+      flag: JAPANflag
     },
     {
       coin: 'Peso Argentino (ARS)',
       value: pesoArgentinoARS,
       borderRadius: '10px',
       background: 'linear-gradient(92deg, #74ACDF 52.60%, #F6B40E 100%)',
-      textColor: 'white'
+      textColor: 'white',
+      flag: ARSflag
     },
     {
       coin: 'Franco-suíço (CHF)',
       value: francoSuicoCHF,
       borderRadius: '10px',
       background: '#DA291C',
-      textColor: 'white'
+      textColor: 'white',
+      flag: SUICAflag
     }
   ]
   return (
@@ -116,9 +216,14 @@ export default function Home() {
         <div className={styles.lowerSection}>
         <div className={styles.cardsContainer}>
             {cards.map(card => (
-              <div className={styles.card} style={{ background: card.background, color: card.textColor }}>
-                <p>{card.coin}</p>
-                <p>{card.value}</p>
+              <div className={styles.card} style={{ background: card.background, color: card.textColor, borderRadius: card.borderRadius, boxShadow: card.boxShadow }}>
+                <div className={styles.flag}>
+                  <Image className={styles.flag} src={card.flag} alt="flag" />
+                </div>
+                <div className={styles.information}>
+                <p className={styles.coin}>{card.coin}</p>
+                <p className={styles.value}>{card.value}</p>
+                </div>
               </div>
             ))}
           </div>
